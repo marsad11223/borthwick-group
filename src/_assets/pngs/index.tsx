bc9f0c48ade9111130fb39e009c3fc0c08a5f6fb
Marsad Ghanvi <marsad11223@gmail.com>
2024-11-14 16:40:12 +0500

    aboutus section responsive

--- src/_assets/pngs/index.tsx
+++ src/_assets/pngs/index.tsx
@@ -1,7 +1,19 @@
 import SolarPlate from "./solarPlate.png";
+import BoilerReplacement from "./energy-efficiency/boilerReplacement.png";
+import CentralHeating from "./energy-efficiency/centralHeating.png";
+import HeaterReplacement from "./energy-efficiency/heaterReplacement.png";
+import HeatPump from "./energy-efficiency/heatPump.png";
+import HomeInsulation from "./energy-efficiency/homeInsulation.png";
+import SolarPower from "./energy-efficiency/solarPower.png";
 
 const pngs = {
   SolarPlate,
+  BoilerReplacement,
+  CentralHeating,
+  HeaterReplacement,
+  HeatPump,
+  HomeInsulation,
+  SolarPower,
 };
 
 export default pngs;
--- src/app/home/_components/OurServices.tsx
+++ src/app/home/_components/OurServices.tsx
@@ -1,9 +1,62 @@
-// import pngs from "@/_assets/pngs";
+import pngs from "@/_assets/pngs";
 import { color, font } from "@/app/utils/themes";
-import { Box, Divider, Typography } from "@mui/material";
-// import Image from "next/image";
+import { Box, Divider, Grid, Typography } from "@mui/material";
+import Image, { StaticImageData } from "next/image";
+
+// Define the type for the props
+interface OurServicesCardProps {
+  imageSrc: StaticImageData;
+  title: string;
+  description: string;
+  alignment: string;
+}
 
 export default function OurServices() {
+  const servicesData = [
+    {
+      imageSrc: pngs.HeatPump,
+      title: "FREE Air Source Heat Pump",
+      description:
+        "Access the latest renewable technology with a Free Air Source Heat Pump.",
+      alignment: "flex-start",
+    },
+    {
+      imageSrc: pngs.HeatPump,
+      title: "FREE Air Source Heat Pump",
+      description:
+        "Access the latest renewable technology with a Free Air Source Heat Pump.",
+      alignment: "center",
+    },
+    {
+      imageSrc: pngs.HeatPump,
+      title: "FREE Air Source Heat Pump",
+      description:
+        "Access the latest renewable technology with a Free Air Source Heat Pump.",
+      alignment: "flex-end",
+    },
+    {
+      imageSrc: pngs.HeatPump,
+      title: "FREE Air Source Heat Pump",
+      description:
+        "Access the latest renewable technology with a Free Air Source Heat Pump.",
+      alignment: "flex-start",
+    },
+    {
+      imageSrc: pngs.HeatPump,
+      title: "FREE Air Source Heat Pump",
+      description:
+        "Access the latest renewable technology with a Free Air Source Heat Pump.",
+      alignment: "center",
+    },
+    {
+      imageSrc: pngs.HeatPump,
+      title: "FREE Air Source Heat Pump",
+      description:
+        "Access the latest renewable technology with a Free Air Source Heat Pump.",
+      alignment: "flex-end",
+    },
+  ];
+
   return (
     <>
       <Box
@@ -15,26 +68,17 @@ export default function OurServices() {
           height: "100%",
           minHeight: { sm: "100vh", xl: "auto" },
           backgroundColor: color.lightGreen,
+          flexDirection: "column",
         }}
       >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: { xs: "center", lg: "space-between" },
-            alignItems: { xs: "flex-start", lg: "center" },
-            flexDirection: { xs: "column", lg: "row" },
-            gap: { xs: "50px", lg: "60px", xl: "80px" },
-            maxWidth: "1600px",
-            width: "100%",
-          }}
-        >
+        <Box sx={{ maxWidth: "1600px", width: "100%" }}>
           <Box
             sx={{
               display: "flex",
               flexDirection: "column",
-              justifyContent: "end",
               alignItems: "end",
-              gap: "20px",
+              justifyContent: "flex-end",
+              gap: "30px",
               width: "100%",
             }}
           >
@@ -63,18 +107,130 @@ export default function OurServices() {
                 about us
               </Typography>
             </Box>
-            <Typography
-              sx={{
-                color: color.darkGreen,
-                fontSize: font.headingTertiary,
-                fontWeight: "700",
-              }}
-            >
-              Making Homes Warmer, Greener, and More Efficient
-            </Typography>
+            <Box>
+              <Typography
+                sx={{
+                  color: color.darkGreen,
+                  fontSize: font.headingTertiary,
+                  fontWeight: "700",
+                  textAlign: "end",
+                  lineHeight: font.headingTertiary,
+                }}
+              >
+                Energy Efficiency
+              </Typography>
+              <Typography
+                sx={{
+                  color: color.darkGreen,
+                  fontSize: font.headingTertiary,
+                  fontWeight: "700",
+                  textAlign: "end",
+                }}
+              >
+                Upgrades Made Easy
+              </Typography>
+            </Box>
           </Box>
+          <Grid
+            sx={{
+              justifyContent: "space-between",
+              rowGap: "50px",
+              paddingTop: {
+                xs: "40px",
+                sm: "40px",
+                md: "60px",
+                lg: "80px",
+                xl: "100px",
+              },
+            }}
+            container
+          >
+            {servicesData.map((service, index) => (
+              <OurServicesCard
+                key={index}
+                imageSrc={service.imageSrc}
+                title={service.title}
+                description={service.description}
+                alignment={service.alignment}
+              />
+            ))}
+          </Grid>
         </Box>
       </Box>
     </>
   );
 }
+
+function OurServicesCard({
+  imageSrc,
+  title,
+  description,
+  alignment,
+}: OurServicesCardProps) {
+  return (
+    <Grid
+      sx={{
+        display: "flex",
+        justifyContent: { xs: "center", lg: alignment },
+      }}
+      item
+      xs={12}
+      md={5.5}
+      lg={3.5}
+    >
+      <Box
+        sx={{
+          maxWidth: "370px",
+          width: "100%",
+          maxHeight: "440px",
+          borderRadius: "12px",
+          overflow: "hidden",
+          height: "100%",
+        }}
+      >
+        <Box sx={{ width: "100%", height: "auto", marginBottom: "-2.5px" }}>
+          <Image
+            style={{
+              height: "100%",
+              width: "100%",
+              objectFit: "contain",
+            }}
+            src={imageSrc}
+            alt={title}
+          />
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            backgroundColor: color.white,
+            padding: "16px 32px 32px 32px",
+            textAlign: "justify",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "20px",
+          }}
+        >
+          <Typography
+            sx={{
+              color: color.darkGreen,
+              fontSize: font.headingQuaternary,
+              lineHeight: font.headingQuaternary,
+              fontWeight: "700",
+            }}
+          >
+            {title}
+          </Typography>
+          <Typography
+            sx={{ color: color.grey, fontSize: font.captionTypography }}
+          >
+            {description}
+          </Typography>
+        </Box>
+      </Box>
+    </Grid>
+  );
+}
+
+export { OurServicesCard };
