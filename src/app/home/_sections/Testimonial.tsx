ca96e6b26576fdd8fbd7eb032f4f823f5110fdaa
naqash <contact@pulsetechnologysolutions.co.uk>
2024-11-22 17:03:37 +0000

    test

--- src/app/home/_sections/Testimonial.tsx
+++ src/app/home/_sections/Testimonial.tsx
@@ -109,6 +109,8 @@ export default function OurProcess() {
             <Box sx={{ maxWidth: "1200px", width: "100%", marginTop: "40px" }}>
               <Slider {...settings}>
                 <OurProcessCard />
+                <OurProcessCard />
+                <OurProcessCard />
               </Slider>
             </Box>
           </Box>
